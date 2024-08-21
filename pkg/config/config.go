6f0c53e576ba55b3d7720ab7e8a6cbd518161388
Kevin Castillo <kavier.jarvis@gmail.com>
2024-08-21 19:54:04 -0400

    feat: agrega las credenciales de bd en env variables
    Obtiene las configuraciones de la base de datos por medio de las
    variables de entorno .env.
    
    Se deja un ejemplo de que variables está tomando el servicio en el
    archivo .env.example

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -18,8 +18,8 @@ func NewConfig() *Config {
 		ServerPort: getEnv("SERVER_PORT", "8080"),
 		DBHost:     getEnv("DB_HOST", "localhost"),
 		DBPort:     getEnv("DB_PORT", "5432"),
-		DBUser:     getEnv("DB_USER", "postgres"),
-		DBPassword: getEnv("DB_PASSWORD", "password"),
+		DBUser:     getEnv("DB_USER", ""),
+		DBPassword: getEnv("DB_PASSWORD", ""),
 		DBName:     getEnv("DB_NAME", "todolist"),
 	}
 }
